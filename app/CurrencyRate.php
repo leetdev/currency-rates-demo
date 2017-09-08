983d86bb497b3d4101533f2717228cf01512b4f1
Ultraleet <ultraleet@gmail.com>
2017-09-08 17:00:06 +0300

    Fix Postgres error because composite key was not set for CurrencyRate

--- app/CurrencyRate.php
+++ app/CurrencyRate.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class CurrencyRate extends Model
 {
     public $timestamps = false;
+    public $incrementing = false;
+
+    protected $primaryKey = ['date', 'currency'];
     protected $dates = ['date'];
 }
